912c309f2aec2671341ef994f5d09036663b891e
nuanyang <1535391482@qq.com>
2023-03-27 14:02:40 +0800

    second commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hahaha");
         System.out.println("dadada");
-
+        System.out.println("push github");
     }
 }
